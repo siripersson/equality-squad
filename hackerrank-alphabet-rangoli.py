c904d2e3d2501a536a0200d93d6ca4574b4c40b0
Dilken Dilber <dilken.dilber@naktergal.tech>
2022-04-06 23:08:57 +0200

    Hackerrank alphabet rangoli

---
+++ hackerrank-alphabet-rangoli.py
@@ -0,0 +1,13 @@
+
+def rangoli(size):
+    #4(x-1) + 1 Längd
+    #2(x-1) + 1 bredd
+    # loop, \n
+    print(size)
+    result = ""
+    numberOfRows = 2(size-1) +1
+    for x in range(numberOfRows):
+        print(result)
+    print(result)
+
+rangoli(3)
